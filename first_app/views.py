05e4f6a89c08de955b19fb1b2fa140b585566886
muraleo <muraleo80923@gmail.com>
2018-11-07 21:13:09 -0500

    populate script

--- first_app/views.py
+++ first_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 # Create your views here.
 
 def index(req):
-    my_dict = {'insert_me': "Hello I am from the views.py"}
-    return render(req, 'first_app/index.html', context = my_dict)
+    webpages_list = Webpage.objects.order_by('name')
+    # my_dict = {'insert_me': "Hello I am from the views.py"}
+    date_dict = {'access_record':webpages_list}
+    return render(req, 'first_app/index.html', context = date_dict)
